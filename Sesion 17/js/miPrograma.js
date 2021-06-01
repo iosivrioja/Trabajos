73c810025546ac177b7cba2111a8124310d2257a
iosivrioja <iosiv120402@gmail.com>
2021-06-01 08:18:04 -0500

    sesion

---
+++ Sesion 17/js/miPrograma.js
@@ -0,0 +1,7 @@
+function GuardarDatos() {
+    localStorage.nombre = document.getElementById("nombre").value;
+    localStorage.celular = document.getElementById("celular").value;
+}
+function RecuperarDatos() {
+    
+}
